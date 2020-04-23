71f1cb117a7eaa7407f937b9b84d8785fe7eb184
yoktav <iletisimyilmazoktav@gmail.com>
2020-04-23 17:22:22 +0300

    Initial commit

---
+++ twig/functions.js
@@ -0,0 +1,32 @@
+import cx from 'classnames';
+
+import { isProduction } from '../config/env';
+
+export default [
+  {
+    name: 'html_classes',
+    func: (...args) =>
+      cx(...args)
+        // remove "_keys" which added by Twig
+        .split(' ')
+        .filter(k => k !== '_keys')
+        .join(' '),
+  },
+  {
+    name: 'html_attributes',
+    func: obj =>
+      Object.keys(obj)
+        .filter(k => k !== '_keys') // remove "_keys" property which added by Twig
+        .reduce((acc, cur) => {
+          if (typeof obj[cur] === 'boolean') {
+            return obj[cur] ? `${acc} ${cur}` : `${acc}`;
+          }
+          return `${acc} ${cur}="${obj[cur]}"`;
+        }, '')
+        .trim(),
+  },
+  {
+    name: 'is_production',
+    func: () => isProduction,
+  },
+];
